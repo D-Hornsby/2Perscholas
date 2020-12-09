911a450bf68f572e38396b4a5719cb93184d5e1b
D-Hornsby <denahornsby.yahoo.com>
2020-12-08 20:19:34 -0500

    update colors

--- tech/html-and-css/W2HW3crTimer/crTimer.js
+++ tech/html-and-css/W2HW3crTimer/crTimer.js
@@ -1,17 +1,49 @@
 
+/* another try for color change*/
+
+
+let elements = document.getElementsByClassName("circle");
+var  elementsLength = elements.length;
+
+var colors = ["#ff33cc", "#cc00cc", "#cc66ff"];
+var currentColorIndex = 0;
+var colorsLength = colors.length;
+
+
+function colorChange() {
+
+
+  if (currentColorIndex > colorsLength) {
+    currentColorIndex = 0;
+  } else {
+    for (i = 0; i < elements.length; i++)
+      elements[i].style.backgroundColor = colors[currentColorIndex];
+  }
+    currentColorIndex++;
+
+
+    if(currentColorIndex >= colorsLength)
+    currentColorIndex = 0;
+}
 
 
 
 
+
+
+
+
+/*
 document.getElementByID("div").style.backgroundColor = 'red';
 window.setTimeout("yourfunction()",10000);
-
-/*window.addEventListener('click', element);
+*/
+/*
+window.addEventListener('click', element);
 var elements = document.getElementsByClassName("grid");
   function elements(){
 
   for (var i = 0; i < elements.length; i++){
-    elements[i].style.backgroundColor = "pink";
+    elements[i].style.backgroundColor = ();
 
     }
   }
@@ -22,10 +54,10 @@ var elements = document.getElementsByClassName("grid");
 
 /*
 document.grid.addEventListener('click', function(e){
-  var self = this,
-  old_bg = this.style.background;
 
-this.style.background = this.style.background == 'pink'? 'purple': 'blue';
+
+
+this.style.background = this.style.background == ''? 'purple': 'blue';
 setTimeout(function(){
 self.style.background = old_bg;
 }, 1000);
