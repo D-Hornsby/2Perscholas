327b064ada299c11af59116d81b38eaff4d74e7e
D-Hornsby <denahornsby.yahoo.com>
2020-12-25 20:54:36 -0500

    tryed to fix my timer

--- tech/html-and-css/W2HW3crTimer/timer.js
+++ tech/html-and-css/W2HW3crTimer/timer.js
@@ -1,68 +1,99 @@
+//I have a weard thing like a squiggle on ln 99 you cant see it in adom but you can in Vs
+//since the squggle appeared i cant make my code work and i cant get rid of the squiggle
+//what in the world is it and how do i get rid of it
+//line 34 console.log not working
+// when the document is loaded, trigger the "documentLoaded" function
+window.addEventListener('DOMContentLoaded', documentLoaded, false);
 
-console.log("boo");
-/*window.addEventListener('DOMContentLoaded',
-documentLoaded, false);*/
-documentLoaded();
-    var startTime;
-    var limite;
-    var tempor;
+var startTime;
+var limite;
+var temporizador;
 
-  function documentLoaded(){
-    "use strict";
+function documentLoaded() {
+  "use strict";
 
-console.log("document");
-    document.getElementById("clock").addEventListener(
-      "click", Clicked);
+  // listen for mouse clicks on the button
+ //what was given
+ //document.getElementById("btnStart").addEventListener("click", botaoClicked, false);
+//changing for #4. the txtTempo us used, keydown fires with button press. changed the id to add listiner
+document.getElementById("txtTempo").addEventListener("keydown", startTimer, false);
 
-  }
-  function Clicked(evt){
-    "use strict";
-    var input = this.querySelector("input");
-    var label = this.querySelector("div");
+}
+//not the timer dont work
+//this is new an broke it. i couldnt get it to work and had help but still dont work
+//also after adding this is when i got the squiggle
+//function to add listener using txtTempo, keydown function fired when key pressed
 
-    input.value = 10;
+console.log("line 27");
+//line 27 and 39 are working but not 30 and now its broken
+//Ive changed about everything to what others have and still not working
+function startTimer(){
+  console.log("is the timer working?");
+  txtTempo.addEventListener("keydown", function keydown(event){
+    if(event.keyCode === 13){
+      console.log("Is the 13 working for enter?")
+      txtTempo1.focus(); //was told i needed this but not sure why yet
+      txtTempo.removeEventListener('keydown', keydown);
 
-    startTime = new Date();
-
-  input.addEventListener("keydown", function keydown(evt){
-    if (evt.keycode ===13) {
-      limit =parseInt(input.value);
-      label.innerHTML = input.value;
-      label.classList.remove("hide");
-      input.classList.add("hide");
-
-      clearInterval(tempor);
-  tempor = setInterval(updateTime, 1000);
       }
     });
-  }
+
+    //need to add txtTempo1
+    txtTempo1.addEventListener("keydown", function keydown(event){
+  if(event.keyCode === 13){
+
+    txtTempo1.removeEventListener('keydown', keydown);
+
+    }
+  });
+}
+
+console.log("after added function test")
+
+// when we click on the button, we save the current time, and the time limit. We then
+// create a timer to execute the "updateTime" function once a second.
+function botaoClicked() {
+  "use strict";
+
+  startTime = new Date();
+
+  limite = parseInt(document.getElementById("txtTempo").value);
+
+  clearInterval(temporizador);
+  temporizador = setInterval(updateTime, 1000);
+}
 
 function updateTime() {
   "use strict";
 
+  // read the current time
   var currentTime = new Date();
 
-  var elapsed = (currentTime.getTime()-
-startTime.getTime()) / 1000;
+  // calculate how many seconds passed since the start of the timer
+  var elapsed = (currentTime.getTime() - startTime.getTime()) / 1000;
 
-var minutes = Math.floor(elapsed / 60);
-var seonds = Math.floor(elapsed % 60);
+  // convert seconds to minutes and seconds (below 60)
+  var minutos = Math.floor(elapsed / 60);
+  var segundos = Math.floor(elapsed % 60);
 
-//pad with 00
-if (minutes < 10) {
-  minutes = "0" + minutes;
-}
-if(seconds < 10) {
-  seconds = "0" + seconds;
-}
-//show time passed
-document.getElementById("clock").formattedTime
-minutes + ":" + seconds;
-//check if we are above the time limit and set the color of crTimer
-  if (minutes >= limit) {
-    document.getElementById("clock").className = "red";
-  } else{
-    document.getElementById("clock").className = "blue";
-
+  // pad with zeroes on the left to look better
+  if (minutos < 10) {
+    minutos = "0" + minutos;
   }
+  if (segundos < 10) {
+    segundos = "0" + segundos;
+  }
+
+  // show the elapsed time
+  document.getElementById("clock").innerHTML = minutos + ":" + segundos;
+
+  // check if we are above the time limit and set the color of the timer accordingly
+  if (minutos >= limite) {
+    document.getElementById("clock").className = "red";
+  } else {
+    document.getElementById("clock").className = "blue";
+  }
+
 }
+
+
